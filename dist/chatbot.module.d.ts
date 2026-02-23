182cbeac84cc23fb70a9feafc939b4dc53837f84
trinv-apero <trinv@apero.vn>
2026-02-23 14:39:12 +0700

    build

--- dist/chatbot.module.d.ts
+++ dist/chatbot.module.d.ts
@@ -1,2 +1,5 @@
+import { DynamicModule } from '@nestjs/common';
+import { ChatbotConfig } from './config/chatbot-config';
 export declare class ChatbotModule {
+    static register(config: ChatbotConfig): DynamicModule;
 }
--- dist/chatbot.module.js
+++ dist/chatbot.module.js
@@ -5,6 +5,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var ChatbotModule_1;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ChatbotModule = void 0;
 const common_1 = require("@nestjs/common");
@@ -12,10 +13,22 @@ const tokens_1 = require("./tokens");
 const openai_chatbot_provider_1 = require("./providers/openai-chatbot.provider");
 const chat_completions_service_1 = require("./services/chat-completions.service");
 const chatbot_assistant_service_1 = require("./services/chatbot-assistant.service");
-let ChatbotModule = class ChatbotModule {
+let ChatbotModule = ChatbotModule_1 = class ChatbotModule {
+    static register(config) {
+        return {
+            module: ChatbotModule_1,
+            providers: [
+                {
+                    provide: tokens_1.CHATBOT_CONFIG,
+                    useValue: config,
+                },
+            ],
+            exports: [tokens_1.CHATBOT_CONFIG],
+        };
+    }
 };
 exports.ChatbotModule = ChatbotModule;
-exports.ChatbotModule = ChatbotModule = __decorate([
+exports.ChatbotModule = ChatbotModule = ChatbotModule_1 = __decorate([
     (0, common_1.Module)({
         providers: [
             {
@@ -36,7 +49,7 @@ exports.ChatbotModule = ChatbotModule = __decorate([
             chat_completions_service_1.ChatCompletionsService,
             chatbot_assistant_service_1.ChatbotAssistantService,
         ],
-        exports: [tokens_1.AI_PROVIDER, chat_completions_service_1.ChatCompletionsService, chatbot_assistant_service_1.ChatbotAssistantService],
+        exports: [tokens_1.AI_PROVIDER, chat_completions_service_1.ChatCompletionsService, chatbot_assistant_service_1.ChatbotAssistantService, tokens_1.CHATBOT_CONFIG],
     })
 ], ChatbotModule);
 //# sourceMappingURL=chatbot.module.js.map
