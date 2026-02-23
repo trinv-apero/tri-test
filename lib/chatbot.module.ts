281ba702623f536e25b40f6997318c28779f9b40
trinv-apero <trinv@apero.vn>
2026-02-23 14:35:47 +0700

    fix: chatbot module error

--- lib/chatbot.module.ts
+++ lib/chatbot.module.ts
@@ -1,10 +1,11 @@
-import { Logger, Module } from '@nestjs/common';
+import { DynamicModule, Logger, Module } from '@nestjs/common';
 
-import { AI_PROVIDER, CHATBOT_LOGGER } from './tokens';
+import { AI_PROVIDER, CHATBOT_CONFIG, CHATBOT_LOGGER } from './tokens';
 import { OpenAIChatbotProvider } from './providers/openai-chatbot.provider';
 import { ChatCompletionsService } from './services/chat-completions.service';
 import { ChatbotAssistantService } from './services/chatbot-assistant.service';
 import { ChatbotLogger } from './interfaces/logger.interface';
+import { ChatbotConfig } from './config/chatbot-config';
 
 @Module({
   providers: [
@@ -28,8 +29,21 @@ import { ChatbotLogger } from './interfaces/logger.interface';
     ChatCompletionsService,
     ChatbotAssistantService,
   ],
-  exports: [AI_PROVIDER, ChatCompletionsService, ChatbotAssistantService],
+  exports: [AI_PROVIDER, ChatCompletionsService, ChatbotAssistantService, CHATBOT_CONFIG],
 })
-export class ChatbotModule {}
+export class ChatbotModule {
+  static register(config: ChatbotConfig): DynamicModule {
+    return {
+      module: ChatbotModule,
+      providers: [
+        {
+          provide: CHATBOT_CONFIG,
+          useValue: config,
+        },
+      ],
+      exports: [CHATBOT_CONFIG],
+    };
+  }
+}
 
 
